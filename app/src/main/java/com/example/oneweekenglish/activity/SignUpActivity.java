871cac20fbadcd5bdd6818921edcb948110fd76e
nguyenhoang352004 <nguyenhoang35004@gmail.com>
2025-05-15 22:58:37 +0700

    sign in, sign up

---
+++ app/src/main/java/com/example/oneweekenglish/activity/SignUpActivity.java
@@ -0,0 +1,38 @@
+package com.example.oneweekenglish.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.oneweekenglish.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sign_up);
+
+        TextView moveSignIn = findViewById(R.id.moveSignIn);
+        SpannableString content = new SpannableString("You have an account");
+        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
+        moveSignIn.setText(content);
+        moveSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
