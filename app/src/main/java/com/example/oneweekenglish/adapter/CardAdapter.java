f7d1c7afd84510de05dc6c1b72ee032b17eb1a5b
HaDangQuang <hadangquang1408@gmail.com>
2025-05-19 11:22:00 +0700

    Merge with latest version

--- app/src/main/java/com/example/oneweekenglish/adapter/CardAdapter.java
+++ app/src/main/java/com/example/oneweekenglish/adapter/CardAdapter.java
@@ -1,16 +1,26 @@
 package com.example.oneweekenglish.adapter;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.example.oneweekenglish.R;
 
 public class CardAdapter extends BaseAdapter {
 
     private static final int TYPE_IMAGE = 0;
     private static final int TYPE_TEXT = 1;
-    private static final int TOTAL_ITEMS = 6;
+    private static final int TOTAL_ITEMS = 10;
+
+    // Giả sử bạn có một mảng chứa URL hình ảnh
+    private String[] imageUrls = {
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJxo2NFiYcR35GzCk5T3nxA7rGlSsXvIfJwg&s",
+            // Thêm các link hình ảnh tiếp theo...
+    };
 
     @Override
     public int getCount() {
@@ -48,6 +58,20 @@ public class CardAdapter extends BaseAdapter {
             } else {
                 convertView = inflater.inflate(R.layout.card_text, parent, false);
             }
+        }
+
+        if (viewType == TYPE_IMAGE) {
+            ImageView imageView = convertView.findViewById(R.id.imageViewCardImage);
+            if (position < imageUrls.length) {
+                Glide.with(parent.getContext())
+                        .load(imageUrls[position])
+                        .placeholder(R.drawable.image_card) // optional
+                        .error(R.drawable.image_card) // optional
+                        .into(imageView);
+            }
+        } else {
+            TextView textView = convertView.findViewById(R.id.textViewCardText);
+            textView.setText("Text item " + position);
         }
 
         return convertView;
