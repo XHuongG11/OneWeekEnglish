b53152838bd407d6b3ec131177ae3b834720f8e2
nguyenhoang352004 <nguyenhoang35004@gmail.com>
2025-05-23 22:33:20 +0700

    fix match picture

--- app/src/main/java/com/example/oneweekenglish/activity/MatchPictureWithLetterActivity.java
+++ app/src/main/java/com/example/oneweekenglish/activity/MatchPictureWithLetterActivity.java
@@ -17,30 +17,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.oneweekenglish.R;
 import com.example.oneweekenglish.adapter.CardAdapter;
 import com.example.oneweekenglish.model.Word;
+import com.example.oneweekenglish.util.GlobalVariable;
 import com.example.oneweekenglish.util.MusicService;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MatchPictureWithLetterActivity extends AppCompatActivity {
-
-        public static List<Word> getWordList() {
-            return Arrays.asList(
-                    new Word("dog", Arrays.asList("chó"), Arrays.asList("a domesticated carnivorous mammal"), "/dɒɡ/", "https://res.cloudinary.com/dvjxenags/image/upload/v1747663322/dog_t7wtjy.gif", "noun"),
-                    new Word("cat", Arrays.asList("mèo"), Arrays.asList("a small domesticated carnivorous mammal with soft fur"), "/kæt/", "https://res.cloudinary.com/dvjxenags/image/upload/v1747663323/cat_uylk2d.gif", "noun"),
-                    new Word("rabbit", Arrays.asList("thỏ"), Arrays.asList("a small mammal with long ears, soft fur, and a short tail"), "/\u02c8r\u00e6b.\u026at/", "https://res.cloudinary.com/dvjxenags/image/upload/v1747663327/bunny_fgvweu.png", "noun"),
-                    new Word("bear", Arrays.asList("gấu"), Arrays.asList("a large heavy mammal with thick fur and a short tail"), "/be\u0259r/", "https://res.cloudinary.com/dvjxenags/image/upload/v1747663327/bear_dlcsuf.gif", "noun"),
-                    new Word("tiger", Arrays.asList("hổ"), Arrays.asList("a large carnivorous feline mammal with a yellow-brown coat striped with black"), "/\u02c8ta\u026a.\u0261\u0259r/", "https://res.cloudinary.com/dvjxenags/image/upload/v1747663322/tiger_abe5bo.gif", "noun")
-            );
-        }
-        private CardAdapter adapter;
+    private List<Word> wordList;
+    private CardAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match_picture_with_letter);
+        wordList = GlobalVariable.currentLesson.getLearnWordPractice().getWords();
         //tải các thẻ
-        adapter = new CardAdapter(getWordList());
+        adapter = new CardAdapter(wordList);
         GridView cardGridView = findViewById(R.id.cardGridView);
         cardGridView.setAdapter(adapter);
 
@@ -96,7 +89,7 @@ public class MatchPictureWithLetterActivity extends AppCompatActivity {
                     firstPosition = -1;
                 }
 
-                if (adapter.getMatchedCount() == getWordList().size() * 2) {
+                if (adapter.getMatchedCount() == wordList.size() * 2) {
                     Toast.makeText(getApplicationContext(), "Chúc mừng! Bạn đã hoàn thành!", Toast.LENGTH_LONG).show();
                     SoundPool soundPool = new SoundPool.Builder()
                             .setMaxStreams(5)
