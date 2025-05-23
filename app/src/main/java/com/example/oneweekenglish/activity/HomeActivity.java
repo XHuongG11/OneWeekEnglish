82e0ca0ab5f85bb862ee13d44548d2193776d0b5
HaDangQuang <hadangquang1408@gmail.com>
2025-05-23 21:30:21 +0700

    Fix nhac nen

--- app/src/main/java/com/example/oneweekenglish/activity/HomeActivity.java
+++ app/src/main/java/com/example/oneweekenglish/activity/HomeActivity.java
@@ -94,7 +94,7 @@ public class HomeActivity extends AppCompatActivity {
 
 
         // Khởi tạo MediaPlayer
-        mediaPlayer = MediaPlayer.create(this, R.raw.background_meditation);
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music);
         mediaPlayer.setLooping(true); // Nhạc lặp lại
         mediaPlayer.start();
 
